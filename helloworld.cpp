9f6d9d08cf40a0ced8e17edf9c5878d45a33a237
dibyam <dibyam1jalan@gmail.com>
2022-06-17 20:56:39 +0530

    <comment>

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,22 @@
 #include<iostream>
 using namespace std;
 
+void swap(int *a,int *b);
+
 int main() {
-    cout<<"hello world";
-    cout<<"jay";
+    cout<<"hello world"<<endl;
+    cout<<"jay"<<endl;
+
+    int a =1, b=6;
+
+    swap(&a,&b);
+    cout<<a<<endl<<b<<endl;
+
+}
+
+void swap(int *a,int *b)
+{
+    *a = *a + *b;
+    *b = *a - *b;
+    *a = *a - *b; 
 }
